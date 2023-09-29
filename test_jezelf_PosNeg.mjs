c1e20f6009ab127c7ec4f6fe6de4dac62ab316d9
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-09-29 18:30:39 +0200

    Korter maken
    ipv twee keer num1>0, 1 keer maken en in de if een if en if else maken voor num2

--- test_jezelf_PosNeg.mjs
+++ test_jezelf_PosNeg.mjs
@@ -6,30 +6,23 @@ const userInput = readline.createInterface({input, output});
 let num1 = parseFloat(await userInput.question('Wat is je eerste getal?'));
 let num2 = parseFloat(await userInput.question('Wat is je tweede getal?'));
 
-if ((num1 > 0) && (num2 > 0)) {
+if (num1 > 0) {
+    if (num2 > 0){
         console.log('Beide getallen zijn positief.');
     }
-if (num1 > 0) {
-    if (num2 < 0) {
+    else if (num2 < 0) {
         console.log('Het eerste getal is positief en het tweede getal is negatief.');
     }
 }if (num1 < 0) {
     if (num2 > 0) {
-    console.log('Het eerste getal is negatief en het tweede getal is positief.');
-    }
-}if (num1 < 0 ){
-    if (num2 <0 ) {
+        console.log('Het eerste getal is negatief en het tweede getal is positief.');
+    } 
+    else if (num2 < 0) {
         console.log('Beide getallen zijn negatief.');
     }
 }if ((num1 == 0) || (num2 == 0)){
     console.log('Eén van de getallen is nul.');
 }
-process.exit();
 
-/* 
-kon beter geschreven worden door bvb:
-if ((num1 > 0) && (num2 > 0)) {
-        console.log('Beide getallen zijn positief.');
-    }
-}
-*/
+
+process.exit();
